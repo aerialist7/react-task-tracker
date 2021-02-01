b70aa5b00212a6780b3491be08fdf8eef0126f81
aerialist7 <aerialist7@github.com>
2021-02-01 15:03:15 +0400

    Apply `useState` for tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,14 @@
-const tasks = [
-    {
-        id: 1,
-        text: "Doctors Appointment",
-        day: "Feb 5h at 2:30pm",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Meeting at School",
-        day: "Feb 5h at 1:30pm",
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Food Shopping",
-        day: "Feb 5h at 2:30pm",
-        reminder: false,
-    },
-]
+import PropTypes from "prop-types"
 
-const Tasks = () =>
+const Tasks = ({tasks}) =>
     <>{
         tasks.map(task =>
             <h3 key={task.id}>{task.text}</h3>
         )
     }</>
+
+Tasks.propTypes = {
+    title: PropTypes.array,
+}
 
 export default Tasks
