c588be3fb72fd0e173a18005d52add1a9cfd9f49
aerialist7 <aerialist7@github.com>
2021-02-01 15:33:43 +0400

    Use 'react-icons' for Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,11 @@
+import {FaTimes} from "react-icons/fa"
+
 const Task = ({task}) =>
     <div className="task">
-        <h3>{task.text}</h3>
+        <h3>
+            {task.text}
+            <FaTimes style={{color: "darkred"}}/>
+        </h3>
         <p>{task.day}</p>
     </div>
 
