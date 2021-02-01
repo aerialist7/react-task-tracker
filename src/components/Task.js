11b5badf416589d60625854ec2f8787c48593b84
aerialist7 <aerialist7@github.com>
2021-02-01 15:42:09 +0400

    Ability to delete task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,13 @@
 import {FaTimes} from "react-icons/fa"
 
-const Task = ({task}) =>
+const Task = ({task, onDelete}) =>
     <div className="task">
         <h3>
             {task.text}
-            <FaTimes style={{color: "darkred"}}/>
+            <FaTimes
+                style={{color: "darkred"}}
+                onClick={() => onDelete(task.id)}
+            />
         </h3>
         <p>{task.day}</p>
     </div>
