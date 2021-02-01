24acab572c6f566681b7d231e4729c8a4c2874d3
aerialist7 <aerialist7@github.com>
2021-02-01 13:54:46 +0400

    Add default styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import Header from "./Header"
 
-function App() {
-    const name = "Alex"
-
-    return (
-        <div className="container">
-            <Header title={`Hello from react, ${name}!`}/>
-        </div>
-    )
-}
+const App = () => (
+    <div className="container">
+        <Header/>
+    </div>
+)
 
 export default App
