1871b9d954b0f8d92f25cb3a761c3c8a3c76a345
aerialist7 <aerialist7@github.com>
2021-02-01 19:38:04 +0400

    Use header "Add" button for form showing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
 
 const App = () => {
+    const [showAddTask, setShowAddTask] = useState(false)
+
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -46,8 +48,11 @@ const App = () => {
     }
 
     return <div className="container">
-        <Header/>
-        <AddTask onAdd={addTask}/>
+        <Header
+            onAdd={() => setShowAddTask(!showAddTask)}
+            showAdd={showAddTask}
+        />
+        {showAddTask && <AddTask onAdd={addTask}/>}
         {
             tasks.length > 0
                 ? <Tasks
